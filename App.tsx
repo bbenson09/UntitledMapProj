c18749bdb3e9260a401d2fb250883296a5e8cfc9
Bevin Benson <bevinbenson@gmail.com>
2021-05-30 01:27:58 -0400

    Add Firebase config

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 export default function App() {
   return (
@@ -11,6 +13,9 @@ export default function App() {
   );
 }
 
+// TODO (@bevin): Remove API key from .env file before release
+const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -19,3 +24,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: 'untitled-map-project-9d42f.firebaseapp.com',
+  databaseURL: 'https://untitled-map-project-9d42f.firebaseio.com/',
+  projectId: 'untitled-map-project-9d42f',
+  storageBucket: 'untitled-map-project-9d42f.appspot.com',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
